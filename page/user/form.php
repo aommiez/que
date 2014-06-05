5c72cf3841cda90e54f2d2ee2a7dfb3f2971ff8b
robocon <robocon@robocon.(none)>
2014-06-05 15:51:39 +0700

    Add User form

---
+++ page/user/form.php
@@ -0,0 +1,40 @@
+<div class='row-fluid'>
+    <div class='span12'>
+        <div class='page-header'>
+            <h1 class='pull-left'>
+                <i class='icon-edit'></i>
+                <span>User Form</span>
+            </h1>
+        </div>
+    </div>
+</div>
+<div class='row-fluid'>
+    <div class='span12 box'>
+        <div class='box-content'>
+            <form accept-charset="UTF-8" action="#" class="form" method="post" style="margin-bottom: 0;" />
+                <div style="margin:0;padding:0;display:inline">
+                    <input name="utf8" type="hidden" value="&#x2713;" />
+                    <input name="authenticity_token" type="hidden" value="CFC7d00LWKQsSahRqsfD+e/mHLqbaVIXBvlBGe/KP+I=" />
+                </div>
+                <div class='control-group'>
+                    <label class='control-label' for='inputText'>Username</label>
+                    <div class='controls'>
+                        <input class='input-block-level' id='inputText' placeholder='Username' type='text' />
+                    </div>
+                </div>
+                <div class='control-group'>
+                    <label class='control-label' for='inputPassword'>Password</label>
+                    <div class='controls'>
+                        <input class='input-block-level' id='inputPassword' placeholder='Password' type='password' value='' />
+                    </div>
+                </div>
+                <div class='form-actions' style='margin-bottom: 0;'>
+                    <div class='btn btn-primary btn-large'>
+                        <i class='icon-save'></i>
+                        Save
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
