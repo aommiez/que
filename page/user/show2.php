98f9c38ee10c59bc73180db718d4434c80e9c9c7
robocon <robocon@robocon.(none)>
2014-06-09 16:44:45 +0700

    Show new design

---
+++ page/user/show2.php
@@ -0,0 +1,106 @@
+<div class="row-fluid">
+    <div class="span12 box">
+        <div class="box-header red-background">
+            <div class="title">
+                OPD 3
+            </div>
+        </div>
+        <div class="box-content">
+            <div class="row-fluid red-background" style="margin-bottom: 6px;" >
+                <div class="span1" style="padding: 10px 0 0 10px;">
+                    <img src="http://placehold.it/100x100" >
+                </div>
+                <div class="span11 text-left"  style="padding: 5px 0;">
+                    <p><strong>สนิท เย็นเย็น</strong></p>
+                    <p>รอรับยา ถ้ามี remark จะใช้สีแดงแบบนี้</p>
+                    <p>เวลาเข้า 09:03 น.</p>
+                </div>
+            </div>
+            <div class="row-fluid" style="margin-bottom: 6px; background-color: #F8B9B9;">
+                <div class="span1" style="padding: 10px 0 0 10px;">
+                    <img src="http://placehold.it/100x100" >
+                </div>
+                <div class="span11 text-left" style="padding: 5px 0;">
+                    <p><strong>สนั่น เมืองไทย</strong></p>
+                    <p>ถ้าเกินเวลา 30 นาทีจะเป็นสีนี้ แต่จริงๆ จะไม่มี remark</p>
+                    <p>เวลาเข้า 09:11 น.</p>
+                </div>
+            </div>
+            <div class="row-fluid" style="margin-bottom: 6px;">
+                <div class="span1" style="padding: 10px 0 0 10px;">
+                    <img src="http://placehold.it/100x100" >
+                </div>
+                <div class="span11 text-left" style="padding: 5px 0;">
+                    <p><strong>สนั่น หวั่นไหว</strong></p>
+                    <p>เวลาเข้า 09:24 น.</p>
+                </div>
+            </div>
+            <div class="row-fluid" style="margin-bottom: 6px;">
+                <div class="span1" style="padding: 10px 0 0 10px;">
+                    <img src="http://placehold.it/100x100" >
+                </div>
+                <div class="span11 text-left" style="padding: 5px 0;">
+                    <p><strong>สนั่น บ้านเมือง</strong></p>
+                    <p>เวลาเข้า 09:48 น.</p>
+                </div>
+            </div>
+            <div class="row-fluid">
+                <div class="span12 text-center muted-background" style="padding: 10px 0 0 10px;">
+                    <h3 style="margin: 0;">มีต่ออีก 13 คิว</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="span12 box">
+        <div class="box-header red-background">
+            <div class="title">
+                OPD 1
+            </div>
+        </div>
+        <div class="box-content">
+            <div class="row-fluid" style="margin-bottom: 6px;">
+                <div class="span1" style="padding: 10px 0 0 10px;">
+                    <img src="http://placehold.it/100x100" >
+                </div>
+                <div class="span11 text-left" style="padding: 5px 0;">
+                    <p><strong>ชวน หลบหลีก</strong></p>
+                    <p>เวลาเข้า 09:21 น.</p>
+                </div>
+            </div>
+            <div class="row-fluid" style="margin-bottom: 6px;">
+                <div class="span1" style="padding: 10px 0 0 10px;">
+                    <img src="http://placehold.it/100x100" >
+                </div>
+                <div class="span11 text-left" style="padding: 5px 0;">
+                    <p><strong>ชวน หลับนอน</strong></p>
+                    <p>เวลาเข้า 10:02 น.</p>
+                </div>
+            </div>
+            <div class="row-fluid" style="margin-bottom: 6px;">
+                <div class="span1" style="padding: 10px 0 0 10px;">
+                    <img src="http://placehold.it/100x100" >
+                </div>
+                <div class="span11 text-left" style="padding: 5px 0;">
+                    <p><strong>ชวน เล่นเกม</strong></p>
+                    <p>เวลาเข้า 10:14 น.</p>
+                </div>
+            </div>
+            <div class="row-fluid" style="margin-bottom: 6px;">
+                <div class="span1" style="padding: 10px 0 0 10px;">
+                    <img src="http://placehold.it/100x100" >
+                </div>
+                <div class="span11 text-left" style="padding: 5px 0;">
+                    <p><strong>ชวน โดดเรียน</strong></p>
+                    <p>เวลาเข้า 10:23 น.</p>
+                </div>
+            </div>
+            <div class="row-fluid">
+                <div class="span12 text-center muted-background" style="padding: 10px 0 0 10px;">
+                    <h3 style="margin: 0;">มีต่ออีก 5 คิว</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
